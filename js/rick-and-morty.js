109f7fbad507136c4f037c304e94ef420c426ec2
adonai-luque <akluqueprieto@gmail.com>
2021-11-10 16:45:23 -0500

    Set imports to main js file

---
+++ js/rick-and-morty.js
@@ -0,0 +1,11 @@
+export class RickAndMorty {
+  constructor() {
+    this.locations = []
+    this.episodes = []
+    this.characters = []
+  }
+
+  getLocations() {
+    console.log("Get locations")
+  }
+}
--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Inside main.js")
+import { RickAndMorty } from "./js/rick-and-morty.js";
+
+console.log("Inside main.js");
+
+const challenge = new RickAndMorty();
+challenge.getLocations();
